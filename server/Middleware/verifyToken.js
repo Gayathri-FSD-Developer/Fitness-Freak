1f0e59b83e4271a7872142a2eb245dd0c48e53c3
Gayathri <150881569+Gayathri-FSD-Developer@users.noreply.github.com>
2024-06-25 18:57:47 +0530

    Update verifyToken.js

--- server/Middleware/verifyToken.js
+++ server/Middleware/verifyToken.js
@@ -11,13 +11,14 @@ export const verifyToken = async(req,res,next)=>{
         if(!req.headers.authorization) return next(createError(401,"Your not authorized user!"));
         const token = req.headers.authorization.split(" ")[1];
         console.log(token);
-
+    
         if(!token){
             return next(createError(401,"Your not authorized user!"));
         }
         // decode the token to get the user id and other details
         const decode = jwt.verify(token,process.env.JWT)
         // set the decoded user id on req & calling the next() 
+        console.log(decode,"Verifyedtoken");
         req.user = decode;
         // console.log(req.user,"userId");          
         return next()
